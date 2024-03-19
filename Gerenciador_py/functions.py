df555d6d5a5eef67a9c246aaab9ce68d99b50b3a
guilherme nunes <91635144+HAAX3CN@users.noreply.github.com>
2024-03-19 10:53:12 -0300

    Add files via upload

--- Gerenciador_py/functions.py
+++ Gerenciador_py/functions.py
@@ -1,36 +1,70 @@
-#Essa função gera o menu.
+import os
+
+class Conta():
+    contas = {}         # Dicionário que faz o papel de um DB.
+
+    # Método construtor da classe.
+    def __init__(self, name, email, senha):
+        self.__name = name
+        self.__email = email
+        self.__senha = senha
+    
+    # Getter_nome
+    @property
+    def name(self):
+        return self.__name
+    
+    # Setter_nome
+    @name.setter
+    def name(self, nome):
+        self.__name = nome        
+    
+    # Getter_email
+    @property
+    def email(self):
+        return self.__email
+    
+    # Setter_email
+    @email.setter
+    def email(self, email):
+        self.__email = email
+    
+    # Getter_senha
+    @property
+    def senha(self):
+        return self.__senha
+    
+    # Setter_senha
+    @senha.setter
+    def senha(self, senha):
+        self.__senha = senha
+
+    # Método de adição de uma conta no dicionário.
+    @staticmethod
+    def add_conta(nome, email, senha):
+        Conta.contas[nome] = {'email': email, 'senha': senha}
+    
+    # Método que remove uma conta do dicionário.
+    @staticmethod
+    def remove_conta(nome):
+        if nome in Conta.contas:
+            del Conta.contas[nome]
+        else:
+            print("Conta não encontrada!")
+    
+    # Método de apresentação da lista.
+    def show_lista():
+        for nome, dados in Conta.contas.items():
+            print(f"Nome: {nome}, Email: {dados['email']}, Senha: {dados['senha']}")
+
+
+@staticmethod
+def apagar():
+    os.system('cls')
+
 def Menu():
-    print("\nGerenciador de Contas")
-    print("[1]Nova conta")
+    print("[1]Adicionar conta")
     print("[2]Remover conta")
-    print("[0]Sair")
-    escolha = int(input('Digite uma opção: '))
+    print("[3]Lista de contas")
+    escolha = input("Escolha: ")
     return escolha
-
-
-contas = {}
-
-#Essa classe é utilizada para criar uma conta.
-class Conta:
-    def __init__(self, nome, email, senha, obs):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
-        self.obs = obs
-        
-#Essa função adiciona uma conta.
-def Adicionar_conta(nome, email, senha, obs):
-    nova_conta = Conta(nome, email, senha, obs)
-    contas[len(contas) + 1] = nova_conta
-    return print(nova_conta)
-
-#Essa função remove uma conta.
-def Remover_conta(nomes):
-    for id_contas, dado in contas.items():
-        if nomes == dado.nome:
-            del contas[id_contas]
-
-#Essa função mostra todas as contas por
-def Mostrar_contas():
-    for id_contas, dado in contas.items():
-        print(f"Nome: {dado.nome}, Email: {dado.email}, Senha:  {dado.senha}, Obs: {dado.obs}")
--- Gerenciador_py/main.py
+++ Gerenciador_py/main.py
@@ -1,23 +1,50 @@
 import functions as func
+import os
+from time import sleep
 
-contas = {}
+
+func.apagar()
 
 while True:
     
     escolha = func.Menu()
     
-    if escolha == 1:
+    if escolha == '1':
+        func.apagar()
         nome = input("Nome: ")
         email = input("Email: ")
         senha = input("Senha: ")
-        obs = input("Obs: ")
-        func.Adicionar_conta(nome, email, senha, obs)
+        func.Conta.add_conta(nome, email, senha)
+        print("\nConta adicionada com sucesso!")
+        sleep(1)
+        func.apagar()
+
+    elif escolha == '2':
+        func.apagar()
+        nome = input("Nome: ")
+        func.Conta.remove_conta(nome)
+        print("\nConta removida com sucesso!")
+        sleep(1)
+        func.apagar()
+    
+    elif escolha == '3':
+        func.apagar()
+        func.Conta.show_lista()
+        continuar = input("Para voltar ao menu aperte enter.")
+        func.apagar()
         
-    elif escolha == 0:
+        
+
+    elif escolha == '0':
+        func.apagar()
         print('-' * 20)
         print("Programa finalizado!")
         print('-' * 20)
         break
+    
     else:
         print("-" * 22)
         print("Essa opção não existe!")
+        print("-" * 22)
+        
+        
